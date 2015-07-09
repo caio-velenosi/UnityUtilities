bb3aa879643f0f6a2c74c0b3a17564083de4d87f
caio-velenosi <caiovelenosi@MacBook-Pro-de-Caio.local>
2015-07-09 03:11:24 -0300

    ResourcePool
    Added ResourcePool class

---
+++ Utilities/ResourcePool.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcePool 
+{
+	GameObject[] list;
+	int _poolSize;
+	
+	int i;
+	
+	public ResourcePool(int poolSize, string resourceName, Transform parent = null)
+	{
+		_poolSize = poolSize;
+		list = new GameObject[_poolSize];
+		
+		for (i = 0; i < _poolSize; i++)
+		{
+			GameObject temp = (GameObject) GameObject.Instantiate(Resources.Load(resourceName, typeof(GameObject)));
+			temp.SetActive(false);
+			if (parent) temp.transform.parent = parent;
+			list[i] = temp;
+		}
+	}
+	
+	public GameObject FirstAvailable
+	{
+		get
+		{
+			for (i = 0; i < _poolSize; i++)
+			{
+				if (!list[i].activeInHierarchy)
+				{
+					return list[i];
+				}
+			}
+			
+			return null;
+		}
+	}
+	
+	public GameObject Get(int index)
+	{
+		return list[index];
+	}
+}
